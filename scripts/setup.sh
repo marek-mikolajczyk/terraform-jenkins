4553bc7b498072d47f8fb9581b2830f83303cebc
Your Name <you@example.com>
2021-03-09 14:58:14 +0100

    update

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -7,5 +7,5 @@ echo "automation ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/automation
 sudo mkdir -p /home/automation/.ssh
 sudo chmod 700 /home/automation/.ssh
 sudo cp /tmp/id_rsa_ssh_terraform-jenkins.pub /home/automation/.ssh/authorized_keys
-sudo chmod 600 /home/terraform/.ssh/authorized_keys
+sudo chmod 600 /home/automation/.ssh/authorized_keys
 sudo chown -R automation /home/automation/.ssh
